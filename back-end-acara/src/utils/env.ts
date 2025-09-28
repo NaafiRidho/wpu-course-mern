23dcc8f58fad6c33659f6fefe95649a04be446e7
NaafiRidho <naafiridho0505@gmail.com>
2025-09-28 17:03:04 +0700

    feat: payment gateway midtrans

--- back-end-acara/src/utils/env.ts
+++ back-end-acara/src/utils/env.ts
@@ -14,3 +14,6 @@ export const CLIENT_HOST: string = process.env.CLIENT_HOST || "http://localhost:
 export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || "";
 export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
 export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || "";
+export const MIDTRANS_KLIEN_KEY: string = process.env.MIDTRANS_KLIEN_KEY || "";
+export const MIDTRANS_SERVER_KEY: string = process.env.MIDTRANS_SERVER_KEY || "";
+export const MIDTRANS_TRANSACTION_URL: string = process.env.MIDTRANS_TRANSACTION_URL || "";
---
+++ back-end-acara/src/utils/payment.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import {
+    MIDTRANS_SERVER_KEY,
+    MIDTRANS_TRANSACTION_URL
+} from "./env"
+
+export interface Payment {
+    transaction_detail: {
+        order_id: string;
+        gross_amount: number;
+    }
+}
+
+export type TypeReponseMidtrans = {
+    token: string;
+    redirect_url: string;
+}
+
+export default {
+    async createLink(payload: Payment): Promise<TypeReponseMidtrans> {
+        const result = await axios.post<TypeReponseMidtrans>(`${MIDTRANS_TRANSACTION_URL}`, payload, {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Basic ${Buffer.from(`${MIDTRANS_SERVER_KEY}:`).toString("base64")}`,
+            }
+        });
+        if (result.status !== 201) {
+            throw new Error("payment Failed");
+        }
+        return result.data;
+    },
+}
